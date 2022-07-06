6535ce615c417ddee7679090b464aeb3dd898e5e
Achraf Bendella <91100199+Achra-f@users.noreply.github.com>
2022-07-06 21:12:20 +0200

    project setup

---
+++ Flight-Ticket-System/Flight-Ticket-System/Views/AirportsPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Flight_Ticket_System.Views
+{
+    /// <summary>
+    /// Interaction logic for AirportsPage.xaml
+    /// </summary>
+    public partial class AirportsPage : Page
+    {
+        public AirportsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
