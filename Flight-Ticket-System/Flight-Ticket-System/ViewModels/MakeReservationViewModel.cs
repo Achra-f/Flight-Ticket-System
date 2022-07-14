a342019d5d31d2eb61655a091c3ed841fe616cf6
Achraf Bendella <91100199+Achra-f@users.noreply.github.com>
2022-07-14 18:30:21 +0200

    adding viewmodels

---
+++ Flight-Ticket-System/Flight-Ticket-System/ViewModels/MakeReservationViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Flight_Ticket_System.ViewModels
+{
+    public class MakeReservationViewModel : ViewModelBase
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        private int flightNumber;
+        public int FlightNumber
+        {
+            get
+            {
+                return flightNumber;
+            }
+            set
+            {
+                flightNumber = value;
+                OnPropertyChanged(nameof(FlightNumber));
+            }
+        }
+
+        private string flightName;
+        public string FlightName
+        {
+            get
+            {
+                return flightName;
+            }
+            set
+            {
+                flightName = value;
+                OnPropertyChanged(nameof(FlightName));
+            }
+        }
+
+        private DateTime departureTime;
+        public DateTime DepartureTime
+        {
+            get
+            {
+                return departureTime;
+            }
+            set
+            {
+                departureTime = value;
+                OnPropertyChanged(nameof(DepartureTime));
+            }
+        }
+        private DateTime arrivalTime;
+        public DateTime ArrivalTime
+        {
+            get
+            {
+                return arrivalTime;
+            }
+            set
+            {
+                arrivalTime = value;
+                OnPropertyChanged(nameof(ArrivalTime));
+            }
+        }
+
+        public ICommand SubmitCommand { get; }
+        public ICommand CancelCommand { get; }
+
+        public MakeReservationViewModel()
+        {
+
+        }
+    }
+}
